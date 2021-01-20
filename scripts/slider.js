3d09d1994812d5cea423b9b93469ad31ac486365
kaykayyou <71623757+kaykayyou@users.noreply.github.com>
2021-01-20 10:30:06 +0530

    Add files via upload

---
+++ scripts/slider.js
@@ -0,0 +1,74 @@
+$('.owl-main').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    stagePadding: 0,
+    navigation: true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        },
+        1440: {
+            items: 1
+        }
+    }
+})
+
+$('.owl-car').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    stagePadding: 0,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        },
+        1440: {
+            items: 4
+        }
+    }
+})
+$('.owl-car-pro').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    stagePadding: 0,
+    navigation: true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        },
+        1339: {
+            items: 3
+        }
+    }
+})
